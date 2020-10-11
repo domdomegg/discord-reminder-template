0019687febdbd1cf08fb9682b296f43e0868903b
Adam Jones <jones_adam@rocketmail.com>
2020-10-11 01:05:40 +0100

    Improve error handling, logging

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 /** @type {{ botToken: string, channelId: string, events: { date: string, message: string }[] }} */
 const config = require('./config.json');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
 /**
  * Sends a message to the Discord channel. Should only be called once.
  * @param {string} text
  */
 const sendMessage = (text) => {
+    const client = new Discord.Client();
+
     client.on('ready', async () => {
-        console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`Logged in as ${client.user.tag}`);
         const channel = await client.channels.fetch(config.channelId);
+        console.log(`Got channel ${channel.id}`);
         await channel.send(text);
+        console.log('Sent message');
         client.destroy();
     });
     
@@ -22,5 +25,8 @@ const today = (new Date()).toISOString().slice(0, 10);
 const event = config.events.find(event => event.date == today);
 
 if (event) {
+    console.log(`Found message to send: ${event.message}`);
     sendMessage(event.message);
+} else {
+    console.log('No message to send');
 }
